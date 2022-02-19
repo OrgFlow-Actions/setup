d0a3296d7b61f81c933a7ff1901571839e87dde8
Daniel Rosenberg <daniel@orgflow.io>
2022-02-19 17:17:06 +0100

    Removed more duplicated code

--- lib/cli.ts
+++ lib/cli.ts
@@ -4,8 +4,8 @@
 
 import * as io from "@actions/io";
 import * as exec from "@actions/exec";
-import { createWriteStream } from "fs";
-import { devNull } from "os";
+// import { createWriteStream } from "fs";
+// import { devNull } from "os";
 
 export async function getInstalledVersion()
 {
@@ -21,32 +21,13 @@ export async function getInstalledVersion()
 	{
 		try
 		{
-			let stdout: string = "";
-			let stderr: string = "";
-
-			const exitCode = await exec.exec("orgflow", ["--version"], {
-				ignoreReturnCode: true,
-				listeners: {
-					stdout: data => stdout += data.toString().trim(),
-					stderr: data => stderr += data.toString().trim(),
-				}
-			});
-
-			if (exitCode !== 0)
-			{
-				console.log(`'orgflow --version' failed with exit code ${exitCode}; CLI is likely not installed correctly. STDERR: ${stderr}`);
-				return null;
-			}
-
-			console.log(`'orgflow --version' returned '${stdout}'.`);
-
-			installedVersion = stdout;
+			const installedVersion = await execOrgFlow("--version");
+			console.log(`'orgflow --version' returned '${installedVersion}'.`);
 		}
 		catch (error)
 		{
 			console.log("Error while running 'orgflow --version'; CLI is likely not installed correctly.");
 			console.log(error);
-			return null;
 		}
 	}
 
@@ -56,23 +37,8 @@ export async function getInstalledVersion()
 export async function setLicenseKey(licenseKey: string)
 {
 	console.log("Validating license key...");
-
-	let stderr: string = "";
-
 	// Use the stack:list command to set license key (somewhat arbitrary, we currently don't have a better way).
-	const exitCode = await exec.exec("orgflow", ["stack:list", `--licenseKey=${licenseKey}`], {
-		ignoreReturnCode: true,
-		outStream: createWriteStream(devNull), // Output from this command may reveal lots of info and should not end up in workflow logs
-		listeners: {
-			stderr: data => stderr += data.toString().trim(),
-		}
-	});
-
-	if (exitCode !== 0)
-	{
-		throw new Error(`'orgflow --licenseKey' failed with exit code ${exitCode}. STDERR: ${stderr}`);
-	}
-
+	await execOrgFlow("stack:list", `--licenseKey=${licenseKey}`);
 	console.log("License key was successfully validated and saved.");
 }
 
@@ -122,6 +88,7 @@ async function execOrgFlow(commandName: string, ...args: string[])
 		],
 		{
 			ignoreReturnCode: true,
+			//outStream: createWriteStream(devNull), // Output from command may reveal lots of info and should not end up in workflow logs
 			listeners: {
 				stdout: data => stdout += data.toString().trim(),
 				stderr: data => stderr += data.toString().trim(),
